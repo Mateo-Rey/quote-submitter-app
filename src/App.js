d1f3985b7b03cacb552ba2e5e0ef1353790b175c
Mateo-Rey <jrey3darts@gmail.com>
2022-08-15 14:37:45 -0400

    started login

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import "./App.css";
-import AddMessage from "./components/AddMessage";
-import Header from "./components/Header";
-import MessageList from "./components/MessageList";
+// import "antd/dist/antd.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { GetHelpResources } from "./pages/GetHelpResources";
+import Home from "./pages/Home";
+// import { useState } from "react";
+// import { Login } from "./pages/Login";
 
 function App() {
+  // const [isLoggedIn, setIsLoggedIn] = useState(true);
+  // if (!isLoggedIn) {
+  //   return <Login setIsLoggedIn={setIsLoggedIn}/>;
+  // }else
   return (
     <>
-      <div className="App">
-        <Header />
-        <AddMessage />
-        <MessageList />
-      </div>
+      <Router>
+        <div className="App">
+          <header>
+              <Link to="/">Home</Link>&nbsp;
+              <Link to="/resources">Resources</Link>
+          </header>
+          <Routes>
+            <Route path="/resources" element={<GetHelpResources />} />
+            <Route index path="/" element={<Home />} />
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 }
--- src/components/AddMessage.jsx
+++ src/components/AddMessage.jsx
@@ -1,4 +1,4 @@
-import '../styles/Header.css'
+import '../styles/Master.css'
 
 import { useState, useEffect } from "react";
 export default function AddMessage() {
