9d2358359c2f15ff5b223554cbde2a53cc44b62d
Mateo-Rey <jrey3darts@gmail.com>
2022-08-15 17:28:33 -0400

    google sign in

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
 import "./App.css";
-// import "antd/dist/antd.css";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { GetHelpResources } from "./pages/GetHelpResources";
 import Home from "./pages/Home";
-// import { useState } from "react";
-// import { Login } from "./pages/Login";
+import { useState } from "react";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from "firebase/auth";
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAQaHgg_f04XfRG7yZM2ioYpxHJT0BOJ7A",
+  authDomain: "chat-app-98837.firebaseapp.com",
+  projectId: "chat-app-98837",
+  storageBucket: "chat-app-98837.appspot.com",
+  messagingSenderId: "625295234706",
+  appId: "1:625295234706:web:e84ea856a3bd587dd6f84b"
+};
+
+const connect = () => {
+  const app = initializeApp(firebaseConfig);
+  return getAuth(app);
+};
+
+
 
 function App() {
-  // const [isLoggedIn, setIsLoggedIn] = useState(true);
-  // if (!isLoggedIn) {
-  //   return <Login setIsLoggedIn={setIsLoggedIn}/>;
-  // }else
+  const [isLoggedin, setIsLoggedIn] = useState(false);
+
+  const handleLogin = async ( ) => {
+    const auth = connect()
+      const provider = new GoogleAuthProvider()
+      const user = await signInWithPopup(auth, provider)
+          .catch(err => alert(err.message))
+      if(user) {
+          console.log(user)
+          setIsLoggedIn(true)
+  }
+  }
+
+
   return (
     <>
+  
       <Router>
         <div className="App">
           <header>
-              <Link to="/">Home</Link>&nbsp;
-              <Link to="/resources">Resources</Link>
+            <Link to="/">Home</Link>&nbsp;
+            <Link to="/resources">Resources</Link>&nbsp;
+            {!isLoggedin && <button onClick={handleLogin}>Login!</button>}
           </header>
           <Routes>
             <Route path="/resources" element={<GetHelpResources />} />
@@ -28,5 +61,4 @@ function App() {
     </>
   );
 }
-
 export default App;
--- src/components/AddMessage.jsx
+++ src/components/AddMessage.jsx
@@ -1,61 +1,73 @@
-import '../styles/Master.css'
-
+import "../styles/Master.css";
 import { useState, useEffect } from "react";
+
 export default function AddMessage() {
   const [formSubmitted, setFormSubmitted] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
   const [form, setForm] = useState({});
-  useEffect(() => {
-    
-  }, [form]);
+  useEffect(() => {}, [form]);
 
   async function formSubmit(e) {
     e.preventDefault();
-    try{
-    const results = await fetch("https://chat-app-98837.web.app/add-message", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(form),
-    });
-    const data = results.json();
-    setFormSubmitted(true)
-    setErrorMessage('')
-    console.log(data);
+
+    try {
+      const results = await fetch(
+        "https://chat-app-98837.web.app/add-message",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(form),
+        }
+      );
+      const data = results.json();
+      setFormSubmitted(true);
+      setErrorMessage("");
+      console.log(data);
     } catch (error) {
-      console.error(error)
-      setErrorMessage("there was an error submitting your comment" + error.toString())
+      console.error(error);
+      setErrorMessage(
+        "there was an error submitting your comment" + error.toString()
+      );
     }
   }
+
   const updateForm = (event) => {
-    setForm({...form, [event.target.name]: event.target.value})
-  }
-    console.log(form)
-    return (
-      <div className="App">
-        <form onSubmit={formSubmit}>
-          <label>Title</label>
-          <input
-          type='text'
-          name='title'
-          placeholder="your title here..."
-          value = {form.title}
-          >
-
-          </input>
-          <label>Message</label>
-          <input
-            type="text"
-            name='text'
-            placeholder="your message here..."
-            onChange={updateForm}
-            value={form.text}
-          ></input>
-
-        {!formSubmitted && <button className="bottom" id='bottom'>Submit Form</button>}
-        {errorMessage &&<h1>There was an error: <br/>{errorMessage}</h1>}
-        </form> 
-      </div>
-    );
+    setForm({ ...form, [event.target.name]: event.target.value });
   };
+  console.log(form);
+  return (
+    <div className="App">
+      <form onSubmit={formSubmit}>
+        <label>Title</label>
+        <input
+          type="text"
+          name="title"
+          placeholder="your title here..."
+          value={form.title}
+        ></input>
+        <label>Message</label>
+        <input
+          type="text"
+          name="text"
+          placeholder="your message here..."
+          onChange={updateForm}
+          value={form.text}
+        ></input>
+
+        {!formSubmitted && (
+          <button className="bottom" id="bottom">
+            Submit Form
+          </button>
+        )}
+        {errorMessage && (
+          <h1>
+            There was an error: <br />
+            {errorMessage}
+          </h1>
+        )}
+      </form>
+    </div>
+  );
+}
